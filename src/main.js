991a562a87f4f1ecd7268830bbd39343b6be549c
CaldatoLuca <lucacaldato15@gmail.com>
2023-12-19 15:42:02 +0100

    feat: fonts install

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from "vue";
+//!file scss
 import "./assets/scss/main.scss";
+
+//!font open sans
+import "@fontsource/open-sans"; // Defaults to weight 400
+import "@fontsource/open-sans/400.css"; // Specify weight
+import "@fontsource/open-sans/400-italic.css"; // Specify weight and style
+
+//!font heebo
+import "@fontsource/heebo"; // Defaults to weight 400
+import "@fontsource/heebo/400.css"; // Specify weight
+
 import App from "./App.vue";
 
 createApp(App).mount("#app");
