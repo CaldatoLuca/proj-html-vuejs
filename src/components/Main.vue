4786e0e602cfc650798aff0064b1aa245bc787e5
CaldatoLuca <lucacaldato15@gmail.com>
2023-12-19 16:03:38 +0100

    feat: jumbo img and bg

---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<script>
+import MainJumbo from "./MainJumbo.vue";
+export default {
+  components: {
+    MainJumbo,
+  },
+};
+</script>
+
+<template>
+  <!-- jumbotron -->
+  <MainJumbo />
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/components/MainJumbo.vue
@@ -0,0 +1,30 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <!-- !jumbotron -->
+  <section class="jumbo">
+    <div class="container flex justify-content-between align-items-center">
+      <div class="left-jumbo">left</div>
+      <div class="right-jumbo">
+        <img src="../assets/img/header.png" alt="Jumbo image" />
+      </div>
+    </div>
+  </section>
+  <h1>ciao</h1>
+</template>
+
+<style scoped lang="scss">
+@use "../assets/scss/partials/variables" as *;
+.jumbo {
+  padding: 15px 0 190px 0;
+  background: linear-gradient(0deg, $bg-blue-dark 0%, $bg-blue-light 100%);
+  .left-jumbo {
+    width: 30%;
+  }
+  .right-jumbo {
+    width: 50%;
+  }
+}
+</style>
