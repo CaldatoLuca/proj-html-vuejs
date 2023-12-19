c2aa079dc55a7f4290066c4fab3fd3d936a56fc1
CaldatoLuca <lucacaldato15@gmail.com>
2023-12-19 18:27:26 +0100

    feat: getImagePath method

--- src/components/MainSection1Card.vue
+++ src/components/MainSection1Card.vue
@@ -1,13 +1,18 @@
 <script>
 export default {
   props: ["card"],
+  methods: {
+    getImagePath: function (imgPath) {
+      return new URL(imgPath, import.meta.url).href;
+    },
+  },
 };
 </script>
 
 <template>
   <div class="card">
     <div class="card-img">
-      <img :src="card.src" :alt="card.title" />
+      <img :src="getImagePath(card.src)" :alt="card.title" />
     </div>
   </div>
 </template>
