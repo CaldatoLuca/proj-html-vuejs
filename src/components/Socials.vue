2345b00c39271b8bf0a478d3e37672c5288a746c
CaldatoLuca <lucacaldato15@gmail.com>
2023-12-21 12:45:04 +0100

    feat: fontawesome installation

--- src/components/Socials.vue
+++ src/components/Socials.vue
@@ -4,7 +4,9 @@ export default {};
 
 <template>
   <ul class="socials flex">
-    <li class="text-white fw-b">c</li>
+    <li class="text-white fw-b">
+      <font-awesome-icon icon="fa-solid fa-user-secret" />
+    </li>
     <li class="text-white fw-b">c</li>
     <li class="text-white fw-b">c</li>
     <li class="text-white fw-b">c</li>
--- src/main.js
+++ src/main.js
@@ -11,6 +11,12 @@ import "@fontsource/open-sans/400-italic.css"; // Specify weight and style
 import "@fontsource/heebo"; // Defaults to weight 400
 import "@fontsource/heebo/400.css"; // Specify weight
 
+//!fontawesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+library.add(faUserSecret);
+
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
